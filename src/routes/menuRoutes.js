1b82be3967d51c02c3840a6870acd0ad014d1ccd
stevenJCL14 <jean123ciencias@gmail.com>
2025-07-31 10:22:50 -0500

    Update menuRoutes.js

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -4,6 +4,7 @@ const menuController = require('../controllers/menuController');
 const { authenticate, isAdmin } = require('../middlewares/authMiddleware');
 
 router.get('/', menuController.getAllMenus);
+router.get('/:diaSemana', menuController.getMenuByDia);
 router.post('/', authenticate, isAdmin, menuController.createMenu);
 router.put('/:id', authenticate, isAdmin, menuController.updateMenu);
 router.delete('/:id', authenticate, isAdmin, menuController.deleteMenu);
